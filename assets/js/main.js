374f42b282dca4aae5a9054edd35f1977e93a02c
Julio Acuña <urkonn@gmail.com>
2014-01-28 23:20:41 -0600

    Sidenav pinta el menú de la sección que se está viendo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,32 @@
-jQuery(document).ready(function(){
-	//var date = new Date();
-	//date.setTime(date.getTime() + (5 * 1000));
+//jQuery(document).ready(function(){
+//	//var date = new Date();
+//	//date.setTime(date.getTime() + (5 * 1000));
+//
+//	var cookieName = 'visited';
+//	var cookieOptions = {expires: 1, path: '/'};
+//	
+//	$("#verbose").val("");
+//	$("#" + $.cookie(cookieName)).addClass("visited");
+//
+//	$(".navbar .nav > li > a").click(function(){
+//		$("#" + $.cookie(cookieName)).removeClass("visited");
+//		$.cookie(cookieName, $(this).attr("id"), cookieOptions);
+//		$("#" + $.cookie(cookieName)).addClass("visited");
+//	});
+//		
+//	
+//});
 
-	var cookieName = 'visited';
-	var cookieOptions = {expires: 1, path: '/'};
-	
-	$("#verbose").val("");
-	$("#" + $.cookie(cookieName)).addClass("visited");
 
-	$(".navbar .nav > li > a").click(function(){
-		$("#" + $.cookie(cookieName)).removeClass("visited");
-		$.cookie(cookieName, $(this).attr("id"), cookieOptions);
-		$("#" + $.cookie(cookieName)).addClass("visited");
-	});
-		
-	
-});
+
+//
+//jQuery(document).ready(function() {
+//  $('.navbar .navbar-inner > li').each(function() {
+//    var href = $(this).find('a').attr('href');
+//    if (href === window.location.pathname) {
+//      jQuery(this).addClass('visited');
+//    }
+//  });
+//});  
+
+
