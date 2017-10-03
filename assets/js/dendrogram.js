043164a0c94648ee0216ce5fbeafdb592c08290b
Matthew Murray <murface@users.noreply.github.com>
2017-10-03 12:50:46 -0400

    update dendro

--- assets/js/dendrogram.js
+++ assets/js/dendrogram.js
@@ -423,22 +423,22 @@ function change() {
                .duration(700)
                .style("opacity", 1);
                div.text("Visit federal account page")
-               .style("left", (d3.event.layerX) + "px")     
-               .style("top", (d3.event.layerY) + "px");
+               .style("left", (d3.event.layerX -150) + "px")     
+               .style("top", (d3.event.layerY -10) + "px");
          } else if(d.depth===2 ){
              div.transition()
                   .duration(700)
                   .style("opacity", 1);
                   div.text("View federal accounts")
-                  .style("left", (d3.event.layerX) + "px")     
-                  .style("top", (d3.event.layerY) + "px");
+                  .style("left", (d3.event.layerX +10) + "px")     
+                  .style("top", (d3.event.layerY -10) + "px");
           } else if(d.depth===1 ){
               div.transition()
                    .duration(700)
                    .style("opacity", 1);
                    div.text("View agency breakdown")
-                   .style("left", (d3.event.layerX) + "px")     
-                   .style("top", (d3.event.layerY) + "px");
+                   .style("left", (d3.event.layerX +10) + "px")     
+                   .style("top", (d3.event.layerY -10) + "px");
           }
        }
       
