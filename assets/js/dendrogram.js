c1a15b925d9dab46f2f4dbafc367393ce0e54693
Matthew Murray <murface@users.noreply.github.com>
2017-10-05 13:38:21 -0400

    update dendro js

--- assets/js/dendrogram.js
+++ assets/js/dendrogram.js
@@ -191,6 +191,59 @@ function change() {
       svgGroup.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
   }*/
 
+var zoom = d3.behavior.zoom().scaleExtent([1, 8]).on("zoom", zoomed);   
+
+    function zoomed() {
+    svg.attr("transform",
+        "translate(" + zoom.translate() + ")" +
+        "scale(" + zoom.scale() + ")"
+    );
+}
+
+function interpolateZoom (translate, scale) {
+    var self = this;
+    return d3.transition().duration(350).tween("zoom", function () {
+        var iTranslate = d3.interpolate(zoom.translate(), translate),
+            iScale = d3.interpolate(zoom.scale(), scale);
+        return function (t) {
+            zoom
+                .scale(iScale(t))
+                .translate(iTranslate(t));
+            zoomed();
+        };
+    });
+}
+    
+function zoomClick() {
+    var clicked = d3.event.target,
+        direction = 1,
+        factor = 0.2,
+        target_zoom = 1,
+        center = [width / 2, height / 2],
+        extent = zoom.scaleExtent(),
+        translate = zoom.translate(),
+        translate0 = [],
+        l = [],
+        view = {x: translate[0], y: translate[1], k: zoom.scale()};
+
+    d3.event.preventDefault();
+    direction = (this.id === 'zoom_in') ? 1 : -1;
+    target_zoom = zoom.scale() * (1 + factor * direction);
+
+    if (target_zoom < extent[0] || target_zoom > extent[1]) { return false; }
+
+    translate0 = [(center[0] - view.x) / view.k, (center[1] - view.y) / view.k];
+    view.k = target_zoom;
+    l = [translate0[0] * view.k + view.x, translate0[1] * view.k + view.y];
+
+    view.x += center[0] - l[0];
+    view.y += center[1] - l[1];
+
+    interpolateZoom([view.x, view.y], view.k);
+}
+    
+d3.selectAll('button').on('click', zoomClick);
+
 
   // define the zoomListener which calls the zoom function on the "zoom" event constrained within the scaleExtents
   //var zoomListener = d3.behavior.zoom().scaleExtent([0.1, 3]).on("zoom", zoom);
@@ -200,7 +253,7 @@ function change() {
       .attr("width", viewerWidth)
       .attr("height", viewerHeight)
       .attr("class", "overlay")
-      .call(zoomListener);
+      .call(zoom);
 
   // Helper functions for collapsing and expanding nodes.
 
