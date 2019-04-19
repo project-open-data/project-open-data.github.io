2abe76469f74cd9fb9f3453a0c8cfa110f55add3
Bernie van Veen <b.vanveen@vwt.digital>
2019-04-19 21:09:31 +0200

    Zip not working on GCP, removed debug lines. DAT-14

--- gcp-templates/deploy_data_catalog.py
+++ gcp-templates/deploy_data_catalog.py
@@ -1,18 +1,15 @@
 import base64
-import gzip
 import json
 
 
 def generate_config(context):
-    catalog = json.loads(gzip.decompress(base64.b64decode(context.properties['data_catalog'])))
-    print(catalog)
+    catalog = json.loads(base64.b64decode(context.properties['data_catalog']))
     resources = []
 
     for dataset in catalog['dataset']:
         for distribution in dataset['distribution']:
 
             if distribution['format'] == 'blob-storage':
-                print('Append storage bucket ' + distribution['title'])
                 resources.append(
                     {
                         'name': distribution['title'],
