dff87596ffd15698940947f09fa3f7482392a2b8
Bernie van Veen <b.vanveen@vwt.digital>
2019-06-06 21:20:52 +0200

    Removed unused v1.0 examples, added default bucket level permissions. DAT-16

--- gcp-templates/deploy_data_catalog.sh
+++ gcp-templates/deploy_data_catalog.sh
@@ -25,45 +25,184 @@ def find_topic(dataset):
     return None
 
 
+resource_default_policy_bindings = {
+    'blob-storage': {
+        'public': [
+            {
+                'role': 'roles/storage.legacyBucketOwner',
+                'members': [
+                    'projectEditor:{project_id}',
+                    'projectOwner:{project_id}'
+                ]
+            },
+            {
+                'role': 'roles/storage.legacyObjectOwner',
+                'members': [
+                    'projectEditor:{project_id}',
+                    'projectOwner:{project_id}'
+                ]
+            },
+            {
+                'role': 'roles/storage.legacyObjectReader',
+                'members': [
+                    'projectViewer:{project_id}',
+                    'allUsers'
+                ]
+            },
+            {
+                'role': 'roles/storage.legacyBucketReader',
+                'members': [
+                    'projectViewer:{project_id}',
+                    'allUsers'
+                ]
+            }
+        ],
+        'internal': [
+            {
+                'role': 'roles/storage.legacyBucketOwner',
+                'members': [
+                    'projectEditor:{project_id}',
+                    'projectOwner:{project_id}'
+                ]
+            },
+            {
+                'role': 'roles/storage.legacyObjectOwner',
+                'members': [
+                    'projectEditor:{project_id}',
+                    'projectOwner:{project_id}'
+                ]
+            },
+            {
+                'role': 'roles/storage.legacyObjectReader',
+                'members': [
+                    'projectViewer:{project_id}'
+                ]
+            },
+            {
+                'role': 'roles/storage.legacyBucketReader',
+                'members': [
+                    'projectViewer:{project_id}'
+                ]
+            }
+        ],
+        'restricted': [
+            {
+                'role': 'roles/storage.legacyBucketOwner',
+                'members': [
+                    'projectEditor:{project_id}',
+                    'projectOwner:{project_id}'
+                ]
+            },
+            {
+                'role': 'roles/storage.legacyObjectOwner',
+                'members': [
+                    'projectEditor:{project_id}',
+                    'projectOwner:{project_id}'
+                ]
+            },
+            {
+                'role': 'roles/storage.legacyObjectReader',
+                'members': [
+                    'projectViewer:{project_id}'
+                ]
+            },
+            {
+                'role': 'roles/storage.legacyBucketReader',
+                'members': [
+                    'projectViewer:{project_id}'
+                ]
+            }
+        ],
+        'confidential': [
+        ]
+    },
+    'topic': {
+        'public': [],
+        'internal': [],
+        'restricted': [],
+        'confidential': []
+    },
+    'subscription': {
+    }
+}
+
+
+def gather_permissions(access_level, resource_format, project_id):
+    bindings_per_level = resource_default_policy_bindings.get(resource_format, None)
+
+    if bindings_per_level:
+        bindings = bindings_per_level[access_level]
+
+        for binding in bindings:
+            binding['members'] = [member.format(project_id = project_id) for member in binding['members']]
+
+        return bindings
+    else:
+        return None
+
+
+def append_gcp_policy(resource, resource_format, access_level, project_id):
+    permissions = gather_permissions(access_level, resource_format, project_id)
+    if permissions is not None:
+        resource['accessControl'] = {
+            'gcpIamPolicy': {
+                'bindings': permissions
+            }
+        }
+
+
 def generate_config(context):
     resources = []
 
     for dataset in catalog['dataset']:
         for distribution in dataset['distribution']:
-
+            resource_to_append = None
             if distribution['format'] == 'blob-storage':
-                blob_def = {
+                resource_to_append = {
                         'name': distribution['title'],
                         'type': 'storage.v1.bucket'
                     }
+                resource_to_append['properties'] = {}
                 if 'deploymentZone' in distribution:
-                    blob_def['properties'] = {
+                    resource_to_append['properties'] = {
                         'location': distribution['deploymentZone']
                     }
-                resources.append(blob_def)
+                if 'accessLevel' in dataset:
+                    bindings = gather_permissions(dataset['accessLevel'], distribution['format'], context.env['project'])
+                    if bindings:
+                        resource_to_append['properties'].update({
+                            'iamConfiguration': {
+                                #'bindings': bindings,
+                                'bucketPolicyOnly': {'enabled': True}
+                            }
+                        })
+                    append_gcp_policy(resource_to_append, distribution['format'], dataset['accessLevel'], context.env['project'])
             if distribution['format'] == 'topic':
-                resources.append(
-                    {
-                        'name': distribution['title'],
-                        'type': 'pubsub.v1.topic',
-                        'properties': 
-                            {
-                                'topic': distribution['title']
-                            }
-                    }
-                )
+                resource_to_append = {
+                    'name': distribution['title'],
+                    'type': 'pubsub.v1.topic',
+                    'properties':
+                        {
+                            'topic': distribution['title']
+                        }
+                }
+                if 'accessLevel' in dataset:
+                    append_gcp_policy(resource_to_append, distribution['format'], dataset['accessLevel'], context.env['project'])
             if distribution['format'] == 'subscription':
-                resources.append(
-                    {
-                        'name': distribution['title'],
-                        'type': 'pubsub.v1.subscription',
-                        'properties': 
-                            {
-                                'topic': '\$(ref.'+find_topic(dataset)+'.name)',
-                                'subscription': distribution['title']
-                            }
-                    }
-                )
+                resource_to_append = {
+                    'name': distribution['title'],
+                    'type': 'pubsub.v1.subscription',
+                    'properties':
+                        {
+                            'topic': '\$(ref.'+find_topic(dataset)+'.name)',
+                            'subscription': distribution['title']
+                        }
+                }
+                if 'accessLevel' in dataset:
+                    append_gcp_policy(resource_to_append, distribution['format'], dataset['accessLevel'], context.env['project'])
+
+            if resource_to_append:
+                resources.append(resource_to_append)
 
     return {'resources': resources}
 EOF
--- gcp-templates/test.py
+++ gcp-templates/test.py
@@ -1,11 +1,16 @@
 import sys
+import json
 from deploy_data_catalog import generate_config
 
 
 class Context:
     def __init__(self):
         self.properties = {}
+        self.env = {
+            'project': 'my-gcp-project',
+            'project_number': 1000
+        }
 
 context = Context()
 context.properties['data_catalog'] = sys.argv[1]
-print(generate_config(context))
+print(json.dumps(generate_config(context)))
